5fa12e60f92dd1d94ad8da6296fb39825516c1b3
Nathan Ndefo <43999981+nattnice@users.noreply.github.com>
2020-11-25 14:50:57 +0000

    Update index.js
    Updated Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,41 @@
 import React from "react";
-import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink,
-} from "./NavbarElements";
-const Navbar = () => {
-  return (
-    <>
-      <Nav>
-        <NavLink to="/">
-          <h1>Logo here</h1>
-        </NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/courses" activeStyle>
-            Courses
-          </NavLink>
-          <NavLink to="/forum" activeStyle>
-            Forum
-          </NavLink>
-          <NavLink to="/Sign-in" activeStyle>
-            Sign up
-          </NavLink>
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to="/signin">Sign In</NavBtnLink>
-        </NavBtn>
-      </Nav>
-    </>
-  );
-};
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, NavDropdown, Nav, Form, FormControl, Button} from 'react-bootstrap';
 
-export default Navbar;
+
+
+function index() {
+  return (
+    <div className="Index">
+      
+
+  
+
+
+<Navbar bg="primary" variant = "dark" expand="lg">
+  <Navbar.Brand href="#home">EduFree</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+
+</div>
+);
+}
+
+export default index;
