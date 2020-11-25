92a0668a4a75e4692d9e4d8f875b91693af45c44
Nathan Ndefo <43999981+nattnice@users.noreply.github.com>
2020-11-25 15:50:28 +0000

    Update index.js
    Updated navbar names

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,13 +13,16 @@ function index() {
 
 
 <Navbar bg="primary" variant = "dark" expand="lg">
+  <div className="justify-content-center">
   <Navbar.Brand href="#home">EduFree</Navbar.Brand>
+
+  </div>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+      <Nav.Link href="#home">Forum</Nav.Link>
+      <Nav.Link href="#link">Sign In</Nav.Link>
+      <NavDropdown title="Courses" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
