5d3eea15d04fdd80eacd6fd2a5fd28cba6a1eea5
Mir Ashiful Haque <mirashifulhaque@gmail.com>
2020-12-05 18:46:24 +0100

    removing some files

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -25,9 +25,9 @@ class Registration extends React.Component{
             <button type="button" className="btn btn-outline-primary" id="btnChoice">Educator</button>
           </ul>
           <div className="btn-group-vertical" role="group" aria-label="Basic example">
-            <button type="button" className="btn btn-outline-primary"> <img src="Resources/GoogleLogo.png" width={30} height={30} style={{textAlign: 'center'}} />Continue with Google</button>
-            <button type="button" className="btn btn-outline-primary"> <img src="Resources/FacebookLogo.png" width={30} height={30} style={{textAlign: 'center'}} />Continue with Facebook</button>
-            <button type="button" className="btn btn-outline-primary"> <img src="Resources/EmailLogo.png" width={30} height={30} style={{textAlign: 'center'}} />Continue with Email</button>
+            <button type="button" className="btn btn-outline-primary"> <img src="assets/images/GoogleLogo.png" width={30} height={30} style={{textAlign: 'center'}} />Continue with Google</button>
+            <button type="button" className="btn btn-outline-primary"> <img src="assets/images/FacebookLogo.png" width={30} height={30} style={{textAlign: 'center'}} />Continue with Facebook</button>
+            <button type="button" className="btn btn-outline-primary"> <img src="assets/images/EmailLogo.png" width={30} height={30} style={{textAlign: 'center'}} />Continue with Email</button>
           </div>
           <p id="member"><em>Already a member?</em><Link to="/Login" class="nav-link">Sign In</Link></p>
         </div>  
