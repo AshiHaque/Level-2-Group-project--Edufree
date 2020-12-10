3620638acec5ad38d64b9359ae68d438cb2a70f7
Nathan Ndefo <43999981+nattnice@users.noreply.github.com>
2020-12-10 16:40:58 +0000

    nav add corrected, cards in place

--- src/Pages/Content.js
+++ src/Pages/Content.js
@@ -8,17 +8,7 @@ class Content extends React.Component{
       <div className="buttons">
 <Container className="buttonContainer" fluid>
   <Row>
-    <Col lg={4}><Button id="Resouces" variant="primary" size="lg">
-      Resources
-    </Button></Col>
-    <Col lg={4}><Button id= "Classes" variant="primary" size="lg">
-      Classes
-    </Button></Col>
-  </Row>
-  </Container>
-<Container className="ContentContainer" fluid>
-  <Row>
-    <Col lg={3}><Nav defaultActiveKey="/home" className="flex-column">
+  <Col className="navCol" lg={3}><Nav defaultActiveKey="/home" className="flex-column">
   <Nav.Link href="/home">Java</Nav.Link>
   <Nav.Link eventKey="link-1">CSS</Nav.Link>
   <Nav.Link eventKey="link-2">HTML</Nav.Link>
@@ -26,33 +16,43 @@ class Content extends React.Component{
     Disabled
   </Nav.Link>
 </Nav></Col>
-    <Col lg={3}><Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+    <Col lg={4}><Button id="Resources" variant="primary" size="lg" block>
+      Resources
+    </Button></Col>
+    <Col lg={4}><Button id= "Classes" variant="primary" size="lg" block>
+      Classes
+    </Button></Col>
+  </Row>
+  </Container>
+<Container className="ContentContainer">
+  <Row>
+
+    <Col lg={4}><Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="./images/clipboard.png" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>Intro Java(Basics)</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+      All you need to get started with coding in Java!
+      IDE's, Java JDKs and other useful tips to help you get started.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Java(Basics)</Button>
   </Card.Body>
 </Card></Col>
 
-    <Col lg={3}><Card style={{ width: '18rem' }}>
+    <Col lg={4}><Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>Data Types(Java)</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+      Get started with learning the fundamental Java data types.
     </Card.Text>
     <Button variant="primary">Go somewhere</Button>
   </Card.Body>
 </Card></Col>
-    <Col lg={3}><Card style={{ width: '18rem' }}>
+    <Col lg={4}><Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>Arrays(Java)</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
