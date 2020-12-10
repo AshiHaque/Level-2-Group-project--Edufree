eda3879c448e94a9018e07a1f0a6375bc9196c3a
Nathan Ndefo <43999981+nattnice@users.noreply.github.com>
2020-12-10 14:32:50 +0000

    Content page foundation

--- src/Pages/Content.js
+++ src/Pages/Content.js
@@ -1,27 +1,67 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Nav from 'react-bootstrap/Nav'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container, Row, Col, Nav, Button, Card} from 'react-bootstrap'
 
 class Content extends React.Component{
     render(){
     return (
-         
-  <div className="mb-2">
-    <Button variant="primary" size="lg">
+      <div className="buttons">
+<Container className="buttonContainer" fluid>
+  <Row>
+    <Col lg={4}><Button id="Resouces" variant="primary" size="lg">
       Resources
-    </Button>{' '}
-    <Button variant="primary" size="lg">
+    </Button></Col>
+    <Col lg={4}><Button id= "Classes" variant="primary" size="lg">
       Classes
-    </Button>
-    <Nav defaultActiveKey="/home" className="flex-column">
+    </Button></Col>
+  </Row>
+  </Container>
+<Container className="ContentContainer" fluid>
+  <Row>
+    <Col lg={3}><Nav defaultActiveKey="/home" className="flex-column">
   <Nav.Link href="/home">Java</Nav.Link>
-  <Nav.Link eventKey="link-1">Link</Nav.Link>
-  <Nav.Link eventKey="link-2">Link</Nav.Link>
+  <Nav.Link eventKey="link-1">CSS</Nav.Link>
+  <Nav.Link eventKey="link-2">HTML</Nav.Link>
   <Nav.Link eventKey="disabled" disabled>
     Disabled
   </Nav.Link>
-</Nav>
-   
+</Nav></Col>
+    <Col lg={3}><Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card></Col>
+
+    <Col lg={3}><Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card></Col>
+    <Col lg={3}><Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card></Col>
+  </Row>
+  </Container>
   </div>
      
     )
