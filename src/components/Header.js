2db2bf63cb18252a0da28dcff65ba5128101ab86
Nathan Ndefo <43999981+nattnice@users.noreply.github.com>
2020-12-11 12:10:44 +0000

    header-dropdown removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,14 +16,7 @@ class Header extends React.Component{
                 <Navbar bg="light" expand="lg">
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-sm-2">
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown ">
-      <NavDropdown.Item tag={Link} to="/Content">Content</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+                <Link to="/Content" class="nav-link">Courses</Link>
     </Nav>
 
     <Form inline>
@@ -45,7 +38,7 @@ class Header extends React.Component{
     <Nav className="ml-auto">
     <Nav.Link href="#home">Forum</Nav.Link>
     <Link to="/Registration" class="nav-link">Sign In</Link>
-    <Link to="/Content" class="nav-link">Content</Link>
+    
     </Nav>
 
   </Navbar.Collapse>
