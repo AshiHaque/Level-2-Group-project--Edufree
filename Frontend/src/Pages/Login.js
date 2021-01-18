8c975ab0386be18cec8f9aaccf2f0e1a26e1b8a5
Mir Ashiful Haque <mirashifulhaque@gmail.com>
2021-01-18 14:01:42 +0100

    Login backend

--- Frontend/src/Pages/Login.js
+++ Frontend/src/Pages/Login.js
@@ -5,17 +5,34 @@ import axios from "axios";
 
 class Login extends React.Component{
   state={
-    data: {
-      username: "",
-      password: ""
-    }
+      username: '',
+      password: ''
+  
   }
 
-  onClick(){
-    fetch('https://localhost:8080/login').then(response => response.data.map())
-
-    axios.post('https://localhost:8080/login', data)
+ 
+  handleChange = event => {
+    this.setState({ username: event.target.value });
+    this.setState({ password: event.target.value });
   }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    axios.post(`/login`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+
+
   render(){
     return(
     <div>
@@ -34,18 +51,23 @@ class Login extends React.Component{
           </div>
         </div>
         <div className="col-6">
-          <form className="form-signin" id="formSignIn">
+
+
+
+          <form className="form-signin" id="formSignIn"  onSubmit={this.handleSubmit}>
             <label htmlFor="inputEmail" className="sr-only">Email address</label>
-            <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus value={data.username}/>
+            <input type="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={this.handleChange}/>
             <label htmlFor="inputPassword" className="sr-only">Password</label>
-            <input type="password" id="inputPassword" className="form-control" placeholder="Password" required value={data.password} />
+            <input type="password" id="inputPassword" className="form-control" placeholder="Password"  onChange={this.handleChange} />
             <div className="checkbox mb-3">
               <label>
                 <input type="checkbox" defaultValue="remember-me" /> Remember me
               </label>
             </div>
-            <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={this.onClick}>Sign in</button>
+            <button className="btn btn-lg btn-primary btn-block" type="submit" >Sign in</button>
           </form>
+
+
           <p id="notMember"><em>Not a member yet?</em><Link to="/Registration" class="nav-link">Sign up</Link></p>
         </div>  
       </div>
