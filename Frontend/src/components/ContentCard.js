92fb90cd28533fdf936cf46c093914e4143d4973
Nathanndefo <43999981+nattnice@users.noreply.github.com>
2021-02-02 10:29:07 +0000

    card component CSS and styling change

--- Frontend/src/components/ContentCard.js
+++ Frontend/src/components/ContentCard.js
@@ -8,21 +8,50 @@ class ContentCard extends React.Component {
   render() {
     return (
       <div class="m-4">
-        <Card style={{ width: "18rem" ,borderRadius:50}}>
+        <Card style={{ width: "18rem", borderRadius: 50 }}>
           <Card.Img variant="top" src={clipboard} />
           <Card.Body>
-            <Card.Title>Intro Java(Basics) <p style={{background: "lightskyblue" ,borderRadius:50 ,fontSize:15}}>#Notes</p></Card.Title>
+            <Card.Title>
+              Intro Java(Basics){" "}
+              <p
+                style={{
+                  background: "lightskyblue",
+                  borderRadius: 50,
+                  fontSize: 15,
+                }}
+              >
+                #Notes
+              </p>
+            </Card.Title>
             <Card.Text>
               All you need to get started with coding in Java! IDE's, Java JDKs
               and other useful tips.
             </Card.Text>
-            <Button style={{background: "lightskyblue" ,borderRadius:10 ,height:40 ,paddingBottom:60,marginBottom:20,marginLeft:50 }}>
+            <Button
+              style={{
+                background: "lightskyblue",
+                borderRadius: 10,
+                height: 40,
+                paddingBottom: 60,
+                marginBottom: 20,
+              }}
+            >
               <Link to="/ContentTest" class="nav-link">
                 Download
               </Link>
             </Button>
-            <Card class="shadow-lg p-4 mb-4 bg-white"style={{ width: '15rem',textAlign:"central"}} >@Team EduFree</Card>
-            <input  class="star" type="checkbox" title="bookmark page"></input>
+            <Card
+              class="shadow-lg p-4 mb-4 bg-white"
+              style={{ width: "15rem", textAlign: "left" }}
+            >
+              @Team EduFree
+            </Card>
+            <input
+              class="star"
+              id="starContent"
+              type="checkbox"
+              title="bookmark page"
+            ></input>
           </Card.Body>
         </Card>
       </div>
