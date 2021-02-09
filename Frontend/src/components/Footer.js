251cae77e64ab5e65c87efb4306b30291a4836bd
jawahirabdi <70774592+jawahirabdi@users.noreply.github.com>
2021-02-09 15:41:25 +0000

    Slight footer change

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -1,14 +1,46 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-class Footer extends React.Component{
-    render(){
-        return(
+class Footer extends React.Component {
+    render() {
+        return (
             <div>
-                <footer class="footer">
+                <div class="footer">
                     <div class="container-fluid">
-                        <p  style={{paddingTop:30}}> © Copyright 2020 EduFree </p>
+                        <div className="row">
+
+                         
+                            <div className="col">
+                                <h5></h5>
+
+                            </div>
+
+                            <div className="col">
+                                <h5></h5>
+
+                            </div>
+
+                            <div className="col">
+                                <h5></h5>
+
+                            </div>
+
+                            <div className="col">
+                                <h5></h5>
+
+                            </div>
+
+                        </div>
+
+                        
+
+                        <div className="row">
+                            <p className="col-sm">
+                                &copy;{new Date().getFullYear()} EDUFREE |    All rights reserved    
+          </p>
+                        </div>
+
                     </div>
-                </footer>
+                </div>
             </div>
         )
     }
