a590aa6ea21827f4d4f54cb5bd3c74ff972079fd
Nathanndefo <43999981+nattnice@users.noreply.github.com>
2021-02-14 19:02:44 +0000

    container added to homepage and buttons aligned

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import students from "../images/STUDENT-IMM.png";
 
 class Home extends React.Component {
@@ -6,35 +7,48 @@ class Home extends React.Component {
     console.log(this.props.userLoggedIn);
     return (
       <div>
-        <div class="row">
-          <div class="col-6">
-            <img
-              src={students}
-              width="900"
-              height="500"
-              alt="student picture"
-            ></img>
+        <Container className="homeContainer" fluid>
+          <div class="row">
+            <div class="col-6">
+              <img
+                src={students}
+                width="2000"
+                height="500"
+                id="studentpic"
+                alt="student picture"
+              ></img>
+            </div>
+            <div class="col-6">
+              <h1 class="title" id="slogan">
+                {" "}
+                Made for Students,by Students
+              </h1>
+              <p id="maininfo">
+                A platform where anyone can learn for free, with endless
+                resources available to support every student.
+              </p>
+              <p id="howDoesItWork">
+                <em>How does it work?</em>
+              </p>
+              <button
+                size="lg"
+                type="button"
+                id="learnbutton"
+                class="btn btn-primary btn-lg"
+              >
+                Learn
+              </button>
+              <button
+                size="lg"
+                type="button"
+                id="teachbutton"
+                class="btn btn-primary btn-lg"
+              >
+                Teach
+              </button>
+            </div>
           </div>
-          <div class="col-6">
-            <h1 class="title" id="slogan">
-              {" "}
-              Made for Students,by Students
-            </h1>
-            <p id="maininfo">
-              A platform where anyone can learn for free, with endless resources
-              available to support every student.
-            </p>
-            <p id="howDoesItWork">
-              <em>How does it work?</em>
-            </p>
-            <button type="button" class="btn btn-primary btn-lg">
-              Learn
-            </button>
-            <button type="button" class="btn btn-primary btn-lg">
-              Teach
-            </button>
-          </div>
-        </div>
+        </Container>
       </div>
     );
   }
