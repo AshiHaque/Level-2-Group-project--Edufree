6d35d07200bd3b2f56d52d7b1f7826a224c67251
snehith10 <mintusnehith@gmail.com>
2021-03-04 15:38:57 +0000

    searchbar frontend and backend( still have few things to sort)

---
+++ Backend/project/src/main/java/SearchDbs/SearchController.java
@@ -0,0 +1,25 @@
+package SearchDbs;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import group4.EduFree.userdetails.User;
+
+public class SearchController {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//
+//	}
+//	@CrossOrigin("http://localhost:3000")
+//	@RequestMapping(method=RequestMethod.POST, value="/re")
+//	@Query("select * from user");
+//	public ResponseEntity<?> addUser(@RequestBody User user) {
+//		userDetailsService.addUser(user);
+//		return ResponseEntity.ok("yes");
+	}
+
--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -11,64 +11,166 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Subject from "./Subject";
 import Cookies from "js-cookie";
 
-function NavLoggedIn() {
-  return (
-    <div>
-      <header>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-sm-2">
-              <Subject />
-            </Nav>
+//   function test() {
 
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-auto"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
+//   let textInput = React.createRef();
+  
+//   function handleClick() {
+//     console.log(textInput.current.value);
+//   }
+// }
+//  textInput = React.createRef();
 
-          <Nav className="navbar mx-auto">
-            <Link to="/" class="navbar-brand logo">
-              <img
-                src={logo}
-                width="215"
-                height="75"
-                alt=""
-                loading="lazy"
-              ></img>
-            </Link>
-          </Nav>
 
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Link to="/UploadFile" class="nav-link">
-                <button type="button" class="btn btn-primary btn-lg">
-                  Upload
-                </button>
-              </Link>
-              <Link to="/Forum" class="nav-link">
-                Forum
-              </Link>
-              <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
-              <Link to="/UserProfile" class="nav-link">
-                Profile
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </header>
-    </div>
-  );
-}
 
-function NavGuest() {
-  return (
-    <div>
+// function NavLoggedIn() {
+//   function handleChange(e) {
+//     console.log(e.target.value);
+//   }
+  
+//   return (
+//     <div>
+//       <header>
+//         <Navbar bg="light" expand="lg">
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="mr-sm-2">
+//               <Subject />
+//             </Nav>
+            
+//             <Form inline>
+//               <input
+//                 type="text"
+//                 placeholder="Search"
+//                 className="mr-auto"
+//                 id="txt_search"
+//                 // ref={textInput}
+//                 onChange={handleChange}
+
+//               />
+//               <Button  variant="outline-success">Search</Button>
+//             </Form>
+//           </Navbar.Collapse>
+
+//           <Nav className="navbar mx-auto">
+//             <Link to="/" class="navbar-brand logo">
+//               <img
+//                 src={logo}
+//                 width="215"
+//                 height="75"
+//                 alt=""
+//                 loading="lazy"
+//               ></img>
+//             </Link>
+//           </Nav>
+
+//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="ml-auto">
+//               <Link to="/UploadFile" class="nav-link">
+//                 <button type="button" class="btn btn-primary btn-lg">
+//                   Upload
+//                 </button>
+//               </Link>
+//               <Link to="/Forum" class="nav-link">
+//                 Forum
+//               </Link>
+//               <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
+//               <Link to="/UserProfile" class="nav-link">
+//                 Profile
+//               </Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Navbar>
+//       </header>
+//     </div>
+//   );
+// }
+
+// function NavGuest() {
+//   return (
+//     <div>
+//       <header>
+//         <Navbar bg="light" expand="lg">
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="mr-sm-2">
+//               <Subject />
+//             </Nav>
+
+//             <Form inline>
+//               <FormControl
+//                 type="text"
+//                 placeholder="Search"
+//                 className="mr-auto"
+//               />
+//               <Button variant="outline-success">Search</Button>
+//             </Form>
+//           </Navbar.Collapse>
+
+//           <Nav className="navbar mx-auto">
+//             <Link to="/" class="navbar-brand logo">
+//               <img
+//                 src={logo}
+//                 width="215"
+//                 height="75"
+//                 alt=""
+//                 loading="lazy"
+//               ></img>
+//             </Link>
+//           </Nav>
+
+//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="ml-auto">
+//               <Nav.Link href="/Support">Help</Nav.Link>
+//               <Link to="/Login" class="nav-link">
+//                 Sign In
+//               </Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Navbar>
+//       </header>
+//     </div>
+//   );
+// }
+
+class Header extends React.Component {
+  state={
+    input:"",
+  }
+  handleInput(e){
+    this.setState({input:e.target.value})
+    console.log(this.state.input)
+    e.preventDefault();
+    var input=this.state.input;
+    const search={
+      input,
+    }
+
+
+    fetch("http://localhost:8080/searchDbs/", {
+      method: "POST",
+      body: JSON.stringify(search),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => {
+      window.alert("search done");
+      // this.setState({ redirect: "/" });
+      console.log("search done");
+    })
+    .catch((error) => console.error("Error:", error));
+  }
+
+  // handleChangeUsername = (event) => {
+  //   this.setState({ username: event.target.value });
+  // };
+  render() {
+    return Cookies.get("user") === null ||
+      Cookies.get("user") === undefined ||
+      Cookies.get("user") === "undefined" ? (
+      // <NavGuest />
+      <div>
       <header>
         <Navbar bg="light" expand="lg">
           <Navbar.Collapse id="basic-navbar-nav">
@@ -110,17 +212,62 @@ function NavGuest() {
         </Navbar>
       </header>
     </div>
-  );
-}
-
-class Header extends React.Component {
-  render() {
-    return Cookies.get("user") === null ||
-      Cookies.get("user") === undefined ||
-      Cookies.get("user") === "undefined" ? (
-      <NavGuest />
     ) : (
-      <NavLoggedIn />
+      // <NavLoggedIn />
+      <div>
+      <header>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-sm-2">
+              <Subject />
+            </Nav>
+            
+            <Form inline>
+              <input
+                type="text"
+                placeholder="Search"
+                className="mr-auto"
+                id="txt_search"
+                // ref={textInput}
+                onChange={(e)=>this.handleInput(e)}
+
+              />
+              <Button  variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+
+          <Nav className="navbar mx-auto">
+            <Link to="/" class="navbar-brand logo">
+              <img
+                src={logo}
+                width="215"
+                height="75"
+                alt=""
+                loading="lazy"
+              ></img>
+            </Link>
+          </Nav>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Link to="/UploadFile" class="nav-link">
+                <button type="button" class="btn btn-primary btn-lg">
+                  Upload
+                </button>
+              </Link>
+              <Link to="/Forum" class="nav-link">
+                Forum
+              </Link>
+              <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
+              <Link to="/UserProfile" class="nav-link">
+                Profile
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+    </div>
     );
   }
 }
